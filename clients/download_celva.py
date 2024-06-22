a908b2e99b55932292f325bd07aef4e9d028b38b
bernardo <bernardo.stearns>
2024-06-22 15:48:20 +0100

    minor bug, celva instead of CELVA"

--- clients/download_celva.py
+++ clients/download_celva.py
@@ -3,7 +3,7 @@ sys.path.append("../ll_datasets")
 from celva import CELVA 
 # from .ll_datasets import *
 
-celva_ds = celva()
+celva_ds = CELVA()
 #############################################
 ##  download cleaned_efcamdat from gdrive  ##
 ##     to "./outputs/EFCAMDAT/" folder     ##
