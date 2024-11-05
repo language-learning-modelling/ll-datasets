3c50bb1984327550c0bb4ae478c45b2c1355db7e
berstearns <bernardo.stearns@gmail.com>
2024-11-05 17:30:46 +0000

    adding load_config as utils

--- ll_datasets/__init__.py
+++ ll_datasets/__init__.py
@@ -3,7 +3,7 @@ try:
 except:
     pass
 
-from .utils import agreements_to_mlm_sentences, compress_dict, decompress_dict, dataclass_to_dict
+from .utils import agreements_to_mlm_sentences, compress_dict, decompress_dict, dataclass_to_dict, load_config
 from .models import ModelsDownloader
 from .efcamdat import EFCAMDAT
 from .fce import FCE
