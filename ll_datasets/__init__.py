a321ebdfff4614a0492a0927d897b721eba36c93
berstearns <bernardo.stearns@gmail.com>
2024-11-05 18:07:49 +0000

    trying to resolve circular import frm datatypes and efcamdat client

--- ll_datasets/__init__.py
+++ ll_datasets/__init__.py
@@ -1,12 +1,8 @@
-try:
-    from .c4200m import C4200m 
-except:
-    pass
-
+# from .c4200m import C4200m 
+from .ll_datatypes import TokenizedText, MaskedSentenceStr, Text
 from .utils import agreements_to_mlm_sentences, compress_dict, decompress_dict, dataclass_to_dict, load_config
 from .models import ModelsDownloader
 from .efcamdat import EFCAMDAT
 from .fce import FCE
 from .celva import CELVA 
-from .ll_datatypes import TokenizedText, MaskedSentenceStr, Text
 
