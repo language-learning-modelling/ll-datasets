271074b2dc13488ac53895c0ccd38694c962a0ab
berstearns <bernardo.stearns@gmail.com>
2024-11-06 16:44:59 +0000

    working example of decompressing for reading binary file

--- clients/compressing.py
+++ clients/compressing.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     import json
-    from ll_datasets import compress_dict
+    from ll_datasets import compress_dict, decompress_dict
     # Sample dictionary
     my_dict = {
         "name": "Alice",
@@ -8,13 +8,20 @@ if __name__ == "__main__":
         "city": "Wonderland",
         "hobbies": ["reading", "adventures", "tea parties"]
     }
-    with open("./datasets/EFCAMDAT/cleaned_efcamdat.json") as inpf:
-        my_dict = json.load(inpf)
+    #with open("./datasets/EFCAMDAT/cleaned_efcamdat.json") as inpf:
+    #   my_dict = json.load(inpf)
 
     # Compress the dictionary
     compressed = compress_dict(my_dict)
-    with open("./datasets/EFCAMDAT/cleaned_efcamdat.pycompressed.json","wb") as outf:
+    filepath= "./test.pycompressed.json"
+    with open(filepath,"wb") as outf:
         outf.write(compressed)
+    def read_dataset_zlib_json(filepath):
+        with open(filepath, "rb") as inpf:
+            decompressed = decompress_dict(inpf.read())
+        return decompressed
+    data = read_dataset_zlib_json(filepath)
+    print(data)
     #print("Compressed data:", compressed)
     # Decompress the dictionary
     # decompressed = decompress_dict(compressed)
