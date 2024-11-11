8a2bf309119aa4f94bb83fe1fd4b2eded90d570f
berstearns <bernardo.stearns@gmail.com>
2024-11-11 11:45:08 +0000

    adding c4200m

--- ll_datasets/__init__.py
+++ ll_datasets/__init__.py
@@ -1,8 +1,7 @@
-# from .c4200m import C4200m 
 from .ll_datatypes import TokenizedText, MaskedSentenceStr, Text
+from .c4200m import C4200m 
 from .utils import agreements_to_mlm_sentences, compress_dict, decompress_dict, dataclass_to_dict, load_config
 from .models import ModelsDownloader
 from .efcamdat import EFCAMDAT
 from .fce import FCE
 from .celva import CELVA 
-
--- ll_datasets/c4200m.py
+++ ll_datasets/c4200m.py
@@ -5,6 +5,12 @@ class C4200m:
 
     def download(self, output_folder_fp):
         '''
+            choose download options
+        '''
+        self.download_hf(output_folder_fp)
+
+    def download_hf(self)
+        '''
         downloads the dataset using the huggingface dataset library
         https://huggingface.co/datasets/liweili/c4_200m?row=1
         '''
